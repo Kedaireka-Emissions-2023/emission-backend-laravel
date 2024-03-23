bf0fa6c1e10bcf0823740ef6e8d97640a9335dc3
okyanawang <okyanawang@gmail.com>
2024-03-23 16:27:08 +0700

    fix: port foreign key

--- src/database/migrations/2024_03_23_073010_update_foreign_keys_for_port.php
+++ src/database/migrations/2024_03_23_073010_update_foreign_keys_for_port.php
@@ -9,33 +9,49 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('emissions', function (Blueprint $table) {
-            $table->dropForeign('fk_drones_has_vessels_ports1');
-            $table->dropForeign('fk_drones_has_vessels_vessels1');
-            $table->dropForeign('fk_drones_has_vessels_drones1');
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_ports1')) {
+                $table->dropForeign('fk_drones_has_vessels_ports1');
+            }
+
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_vessels1')) {
+                $table->dropForeign('fk_drones_has_vessels_vessels1');
+            }
+
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_drones1')) {
+                $table->dropForeign('fk_drones_has_vessels_drones1');
+            }
 
             $table->foreign('drone_id', 'fk_drones_has_vessels_ports1')
-                    ->references('id')->on('ports')
-                    ->onDelete('CASCADE')
-                    ->onUpdate('CASCADE');
+                ->references('id')->on('ports')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
 
             $table->foreign('vessel_id', 'fk_drones_has_vessels_vessels1')
-                    ->references('id')->on('vessels')
-                    ->onDelete('CASCADE')
-                    ->onUpdate('CASCADE');
+                ->references('id')->on('vessels')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
 
             $table->foreign('port_id', 'fk_drones_has_vessels_drones1')
-                    ->references('id')->on('drones')
-                    ->onDelete('CASCADE')
-                    ->onUpdate('CASCADE');
+                ->references('id')->on('drones')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
         });
     }
 
     public function down(): void
     {
         Schema::table('emissions', function (Blueprint $table) {
-            $table->dropForeign('fk_drones_has_vessels_drones1');
-            $table->dropForeign('fk_drones_has_vessels_vessels1');
-            $table->dropForeign('fk_drones_has_vessels_ports1');
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_drones1')) {
+                $table->dropForeign('fk_drones_has_vessels_drones1');
+            }
+
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_vessels1')) {
+                $table->dropForeign('fk_drones_has_vessels_vessels1');
+            }
+
+            if (Schema::hasTable('emissions') && Schema::hasForeignKey('emissions', 'fk_drones_has_vessels_ports1')) {
+                $table->dropForeign('fk_drones_has_vessels_ports1');
+            }
         });
     }
 };
