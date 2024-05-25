5b6527d62ffd8cfc675d76bc5135ab11827dc893
okyanawang <okyanawang@gmail.com>
2024-05-25 15:40:47 +0700

    fix: Rate Limiter

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -27,7 +27,7 @@ class RouteServiceProvider extends ServiceProvider
         Route::pattern('path', '.*');
 
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(600)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function () {
